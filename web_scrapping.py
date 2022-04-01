1c9c52a877a0d4dabe7cc752ad35dcbe4ad31c9c
Dehesa97 <chongjc_2009@hotmail.es>
2022-04-01 17:54:59 +0100

    Initial code

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,20 +1,21 @@
 
 
 #TODO: Web scraped data from Internet using APIS
-
 #Scrape numeric data using the APIS
-
 #COINGECKO
 import requests
 import json
 import os
 
+#current Bitcoin price
 coins1 = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd").text
 coins = json.loads(coins1)
-for coin in coins:
-    print(coin, ' : ', coins[coin])
 price = int(coins["bitcoin"]["usd"])
 print(price)
 
-os.system(f"notify-send  \"the price of BTC is {price}\"")
+#Scrape text data using the APIS
+
+#------------------------------------------------------------
+
 #TODO: Web scraped data from Internet using alternative methods.
+
