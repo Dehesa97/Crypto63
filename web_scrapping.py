5e192c9e8ab72fc95f81c03f7fa10da58ecc3d46
Dehesa97 <chongjc_2009@hotmail.es>
2022-04-02 00:19:04 +0100

    No comment

--- web_scrapping.py
+++ web_scrapping.py
@@ -11,7 +11,16 @@ import os
 coins1 = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd").text
 coins = json.loads(coins1)
 price = int(coins["bitcoin"]["usd"])
-print(price)
+
+#Bitcoin prices from 1st January 2018 till now
+from datetime import datetime, timedelta
+
+coins2 = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1514764800&to=1648851926000").text
+coins3 = json.loads(coins2) # dictionary containing all the prices from 1/1/18 till now in json format
+UNIX_date = (coins3["prices"][0][0])/1000 #UNIX time needed in seconds so converts from milliseconds to second
+print(datetime.fromtimestamp(int(UNIX_date)).strftime('%Y-%m-%d %H:%M:%S')) #date in specific format obtained
+#TODO: get all prices printed along with corresponding date
+
 
 #Scrape text data using the APIS
 
