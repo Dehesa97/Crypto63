e678273da6f595035b9d5c0bc335714441a41fcd
kaskiris <sgakaski@liverpool.ac.uk>
2022-04-17 19:10:40 +0100

    Update selenium1.py

--- selenium1.py
+++ selenium1.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
+
 
 options = webdriver.ChromeOptions()
 options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
@@ -8,10 +10,16 @@ PATH = r"C:\Users\Andre\Downloads\chromedriver_win32\chromedriver.exe"
 print("hello2")
 driver = webdriver.Chrome(chrome_options = options, executable_path =PATH)
 print("hello3")
-driver.get("https://cointelegraph.com/")
+driver.get("https://coinmarketcal.com/en/news/microstrategy-buys-an-additional-190-million-worth-of-bitcoin")
 print("hello4")
-time.sleep(50)
+link = driver.find_element_by_link_text("View coin")
+link.click()
+#time.sleep(50)
+#driver.manage().window().maximize();
+#driver.findElement(By.id("News")).click();
+
 print("hello5")
+
 page_source = driver.page_source
 print("hello6")
 #print(page_source)
