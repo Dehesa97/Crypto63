74d078c2422e2beee3d9d19bd12d86b12fa16d7e
Dehesa97 <63833370+Dehesa97@users.noreply.github.com>
2022-04-17 22:42:11 +0100

    Updates on selenium

--- selenium1.py
+++ selenium1.py
@@ -1,28 +1,49 @@
+from xml.dom.minidom import Element
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
+#----------Programming Selenium on CoinTelegraph-----------------------------------------------
+def selenium_CoinTelegraph():
+
+    current_date = datetime.today().strftime('%Y-%m-%d')
+
+    #Point to the location of the chrome.exe file
+    options = webdriver.ChromeOptions()
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    
+    #Point to the location of the chromedriver.exe file
+    PATH = r"\\mfs01\user05\SGYJAVIE\Documents\chromedriver_win32\chromedriver.exe"
+    driver = webdriver.Chrome(chrome_options = options, executable_path =PATH)
+    
+    driver.maximize_window()
+    
+    driver.get("https://cointelegraph.com/tags/bitcoin")
+    time.sleep(10)
+   
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    #print(soup.prettify())
+
+    #Looks for articles published in the previous day so if its 12/2/2022 it will look for articles published on 11/2/2022
+    articles = soup.find_all("article")
+    #print(articles)
+    for article in articles:
+        print("--------------------------------")
+        #print(article.prettify())
+        #date_posted = article.find("class")
+        #print(date_posted)
+        if article.has_attr('datetime'):
+            print(article['datetime'])
+        else:
+            print('no attribute present')
+        
 
 
-options = webdriver.ChromeOptions()
-options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-print("hello1")
-PATH = r"C:\Users\Andre\Downloads\chromedriver_win32\chromedriver.exe"
-print("hello2")
-driver = webdriver.Chrome(chrome_options = options, executable_path =PATH)
-print("hello3")
-driver.get("https://coinmarketcal.com/en/news/microstrategy-buys-an-additional-190-million-worth-of-bitcoin")
-print("hello4")
-link = driver.find_element_by_link_text("View coin")
-link.click()
-#time.sleep(50)
-#driver.manage().window().maximize();
-#driver.findElement(By.id("News")).click();
-
-print("hello5")
-
-page_source = driver.page_source
-print("hello6")
-#print(page_source)
-print("hello7")
-soup = BeautifulSoup(page_source, 'html.parser')
-print(soup.prettify())
+#--------------------------------------
+selenium_CoinTelegraph()
